1e40b38f1f413fb4771da8f49f35769f4e3ad258
Matthew.Wong <matthew.wong@ticketmaster.com>
2018-09-30 15:20:07 -0700

    hello ios

---
+++ helloios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Matthew Wong on 9/30/18.
+//  Copyright © 2018 Matthew Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
